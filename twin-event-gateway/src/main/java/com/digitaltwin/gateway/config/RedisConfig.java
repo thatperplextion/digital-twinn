14f6dd002a15969218938eb9058dc292ac9c3b98
thatperplextion <junaidforsure.98@gmail.com>
2026-01-22 17:51:29 +0530

    redis configues

--- twin-event-gateway/src/main/java/com/digitaltwin/gateway/config/RedisConfig.java
+++ twin-event-gateway/src/main/java/com/digitaltwin/gateway/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.digitaltwin.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     
     @Bean
+    @Primary
     public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
             ReactiveRedisConnectionFactory connectionFactory) {
         
