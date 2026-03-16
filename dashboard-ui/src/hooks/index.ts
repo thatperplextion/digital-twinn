3a1c7a1e69a11e4989b6ad7bc62c1e11054034fe
thatperplextion <junaidforsure.98@gmail.com>
2026-03-17 02:06:18 +0530

    updated ts

--- dashboard-ui/src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-// Minor change for commit history
-export * from './useQueries';
-export * from './useWebSocket';
-export * from './useQueries';
-export * from './useWebSocket';
--- dashboard-ui/tailwind.config.js
+++ dashboard-ui/tailwind.config.js
@@ -6,7 +6,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
   darkMode: 'class',
   theme: {
